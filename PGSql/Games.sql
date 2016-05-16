5267fd8dc08c5cff65c47ea6621502a81586f404
Kirill <Kirill@DESKTOP-JQ98T2H>
2016-05-16 23:51:17 +0300

    v0.1

---
+++ PGSql/Games.sql
@@ -0,0 +1,20 @@
+﻿-- Table: dbo."Games"
+
+-- DROP TABLE dbo."Games";
+
+CREATE TABLE dbo."Games"
+(
+  "GameId" serial NOT NULL,
+  "Name" character(100) NOT NULL,
+  "Description" character(500) NOT NULL,
+  "Category" character(50) NOT NULL,
+  "Price" numeric(16,2) NOT NULL,
+  CONSTRAINT "Primary Key" PRIMARY KEY ("GameId")
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE dbo."Games"
+  OWNER TO postgres;
+COMMENT ON TABLE dbo."Games"
+  IS 'Таблица игр';
